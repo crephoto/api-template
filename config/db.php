01d42c7fb856de874b701dec70f70bae98e47a2a
Paul Statezny <paulstatezny@users.noreply.github.com>
2014-10-30 12:48:35 -0700

    Refs #68 - Add documentation for db config. Add fields that need to be filled out.

--- config/db.php
+++ config/db.php
@@ -4,13 +4,23 @@
  * Settings
  * ========
  *
- * driver  The database driver to be used
- * charset The character set to be used
- * socket  Custom socket to be used for connections (false to use default)
+ * driver    The database driver to be used
+ * charset   The character set to be used
+ * socket    Custom socket to be used for connections (false to use default)
+ * database  Name of the database
+ * username  Database username
+ * password  Database user's password
+ * hostname  The location of the database host (Usually localhost when not on production)
+ * port      The port of the database host (Default MySQL port is 3306)
  */
 
 return [
-    'driver'  => 'Pdo_Mysql',
-    'charset' => 'utf-8',
-    'socket'  => false,
+    'driver'   => 'Pdo_Mysql',
+    'charset'  => 'utf-8',
+    'socket'   => false,
+    'database' => null,
+    'username' => null,
+    'password' => null,
+    'hostname' => null,
+    'port'     => null,
 ];
--- config/production/db.php
+++ config/production/db.php
@@ -1,5 +1,23 @@
 <?php
 
+/**
+ * Settings
+ * ========
+ *
+ * driver    The database driver to be used
+ * charset   The character set to be used
+ * socket    Custom socket to be used for connections (false to use default)
+ * database  Name of the database
+ * username  Database username
+ * password  Database user's password
+ * hostname  The location of the database host (Usually localhost when not on production)
+ * port      The port of the database host (Default MySQL port is 3306)
+ */
+
 return [
-    'username' => 'production',
+    'database' => null,
+    'username' => null,
+    'password' => null,
+    'hostname' => null,
+    'port'     => null,
 ];
