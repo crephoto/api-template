56ca037e8a67150f3cfd08f9c5aaae386a9031b3
Paul Statezny <paulstatezny@users.noreply.github.com>
2014-10-30 13:04:17 -0700

    Refs #68 - Add remaining missing documentation.
    Add social-login override for production to remove reference to localhost.

--- config/development/init.php
+++ config/development/init.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Settings
+ * ========
+ *
+ * version The version of the application
+ * debug   When true, exceptions and errors will be displayed to the browser
+ */
+
 return [
     'debug' => true,
 ];
